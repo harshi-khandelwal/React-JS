afbe200470b466d4683b227d3c84baa004da4f32
harshi-khandelwal <harshikhandelwal.12a@gmail.com>
2025-05-28 21:18:48 +0530

    day-1

---
+++ day 1/day1/01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./one"
+
+function App() {
+  const username = 'hello'
+  return (
+    <>
+    <Chai/>
+    <h1>harshi khandelwal {username} </h1>
+    </>
+  )
+}
+
+export default App
---
+++ day 1/day1/01vitereact/src/one.jsx
@@ -0,0 +1,5 @@
+function Chai(){
+    return(<h3>cahi is ready</h3>)
+}
+
+export default Chai
