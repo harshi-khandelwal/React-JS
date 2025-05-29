35ffbeabf420a0f8eae38e888125a9f606c275f0
harshi-khandelwal <harshikhandelwal.12a@gmail.com>
2025-05-29 22:33:36 +0530

    day-2

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Card from './components/card'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-red-500 p-4 rounded-2xl bg-amber-400'>Hello world</h1>
+      <Card/>
+      <Card/>
+    </>
+  )
+}
+
+export default App
